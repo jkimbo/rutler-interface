b834c6a0cfb1d814cfe4d86c583a29d6879e559f
Jonathan Kim <jkimbo@gmail.com>
2011-11-23 13:49:04 +0000

    Added jquery and client parses json

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,11 @@
 var socket = io.connect("http://localhost");
-var server = io.connect("http://localhost:8000");
-
-server.on('message', function(data) {
-    console.log('server', data);
-    server.emit('confirmation', { recieved: data.message });
-});
 
 socket.on('connect', function() {
     console.log('Connected');
 });
 
 socket.on('message', function(data) {
-    console.log(data);
+    var something = $.parseJSON(data.message.replace('\n',''));
+    console.log(something);
+    $('#messagelist').append($('<li>').text(data.message));
 });
